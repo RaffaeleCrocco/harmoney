1ea98d715f8a7f8dc8ea6caa1e9c2a5f4522a978
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-20 05:50:25 +0200

    Update Settings.jsx

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BASEURL } from "../config";
+import CoinAnimation from "../components/CoinAnimation";
 
 const Settings = ({ userId }) => {
   const token = localStorage.getItem("token");
@@ -31,27 +32,9 @@ const Settings = ({ userId }) => {
       <div className="w-full border-b border-zinc-200 py-8 px-8 font-semibold text-2xl flex justify-between items-center gap-2">
         <div className="me-auto">Impostazioni</div>
       </div>
-      <div className="min-w-full h-[60vh] flex p-8">
-        <ul className="max-w-xs flex flex-col">
-          <li className="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-            Impostazioni del profilo
-          </li>
-          <li
-            onClick={() => handleDeleteUser()}
-            className="cursor-pointer inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg"
-          >
-            Danger: elimina profilo
-          </li>
-          <li className="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-            -
-          </li>
-          <li className="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-            -
-          </li>
-          <li className="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-            -
-          </li>
-        </ul>
+      <div className="min-w-full h-[60vh] flex flex-col items-center p-8">
+        <p className="text-xs">Sto lavorando a questa pagina.</p>
+        <CoinAnimation />
       </div>
     </div>
   );
