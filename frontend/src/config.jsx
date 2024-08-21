e09c2f49b8385912a9a2f814ca6b873e3f15c7d5
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-21 22:07:30 +0200

    env fix

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,16 +1,1 @@
-// src/config.jsx
-import * as fs from 'fs';
-import * as path from 'path';
-
-const envFilePath = path.resolve(__dirname, '../.env');
-const envContent = fs.readFileSync(envFilePath, 'utf8');
-
-const envVars = {};
-envContent.split('\n').forEach(line => {
-  const [key, value] = line.split('=');
-  if (key && value) {
-    envVars[key.trim()] = value.trim();
-  }
-});
-
-export const BASEURL = envVars.BASEURL; 
+export const BASEURL = import.meta.env.VITE_BASEURL;
