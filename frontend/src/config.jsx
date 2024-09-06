5c84bb57da38eac051cfef841adeceb036f992fc
Raffaele <raffaelecrocco2000@gmail.com>
2024-09-06 14:02:15 +0200

    prop db

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,2 +1,2 @@
-// export const BASEURL = "https://harmoney-api.vercel.app";
-export const BASEURL = "http://192.168.68.113:5551";
+export const BASEURL = "https://harmoney-api.vercel.app";
+// export const BASEURL = "http://192.168.68.113:5551";
